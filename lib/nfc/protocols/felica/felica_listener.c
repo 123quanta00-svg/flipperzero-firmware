ded85d2f77d41bc4476db90e6d87187a2a8e4d40
123quanta00-svg <123quanta00@gmail.com>
2026-06-26 13:35:05 +0800

    test 8008

--- lib/nfc/protocols/felica/felica_listener.c
+++ lib/nfc/protocols/felica/felica_listener.c
@@ -14,9 +14,6 @@
 #define FELICA_LISTENER_REQUEST_SYSTEM_CODE (0x01U)
 #define FELICA_LISTENER_REQUEST_PERFORMANCE (0x02U)
 
-#define FELICA_LISTENER_SYSTEM_CODE_NDEF  (__builtin_bswap16(0x12FCU))
-#define FELICA_LISTENER_SYSTEM_CODE_LITES (__builtin_bswap16(0x88B4U))
-
 #define FELICA_LISTENER_PERFORMANCE_VALUE (__builtin_bswap16(0x0083U))
 
 #define TAG "FelicaListener"
@@ -36,8 +33,11 @@ FelicaListener* felica_listener_alloc(Nfc* nfc, FelicaData* data) {
 
     memcpy(instance->mc_shadow.data, instance->data->data.fs.mc.data, FELICA_DATA_BLOCK_SIZE);
     instance->data->data.fs.state.data[0] = 0;
+
     nfc_config(instance->nfc, NfcModeListener, NfcTechFelica);
-    const uint16_t system_code = *(uint16_t*)data->data.fs.sys_c.data;
+
+    // === Force System Code 0x8008 for ALL FeliCa emulation ===
+    const uint16_t system_code = 0x8008;
     nfc_felica_listener_set_sensf_res_data(
         nfc, data->idm.data, sizeof(data->idm), data->pmm.data, sizeof(data->pmm), system_code);
 
@@ -46,17 +46,12 @@ FelicaListener* felica_listener_alloc(Nfc* nfc, FelicaData* data) {
 
 void felica_listener_free(FelicaListener* instance) {
     furi_assert(instance);
-    furi_assert(instance->tx_buffer);
-
     bit_buffer_free(instance->tx_buffer);
     bit_buffer_free(instance->rx_buffer);
     free(instance);
 }
 
-void felica_listener_set_callback(
-    FelicaListener* listener,
-    NfcGenericCallback callback,
-    void* context) {
+void felica_listener_set_callback(FelicaListener* listener, NfcGenericCallback callback, void* context) {
     UNUSED(listener);
     UNUSED(callback);
     UNUSED(context);
@@ -64,102 +59,7 @@ void felica_listener_set_callback(
 
 const FelicaData* felica_listener_get_data(const FelicaListener* instance) {
     furi_assert(instance);
-    furi_assert(instance->data);
-
     return instance->data;
-}
-
-static FelicaError felica_listener_command_handler_read(
-    FelicaListener* instance,
-    const FelicaListenerGenericRequest* const generic_request) {
-    const FelicaListenerReadRequest* request = (FelicaListenerReadRequest*)generic_request;
-    FURI_LOG_D(TAG, "Read cmd");
-
-    FelicaListenerReadCommandResponse* resp = malloc(
-        sizeof(FelicaCommandResponseHeader) + 1 +
-        FELICA_LISTENER_READ_BLOCK_COUNT_MAX * FELICA_DATA_BLOCK_SIZE);
-
-    resp->header.response_code = FELICA_LISTENER_RESPONSE_CODE_READ;
-    resp->header.idm = request->base.header.idm;
-    resp->header.length = sizeof(FelicaCommandResponseHeader);
-
-    if(felica_listener_validate_read_request_and_set_sf(instance, request, &resp->header)) {
-        resp->block_count = request->base.header.block_count;
-        resp->header.length++;
-    } else {
-        resp->block_count = 0;
-    }
-
-    instance->mac_calc_start = 0;
-    memset(instance->requested_blocks, 0, sizeof(instance->requested_blocks));
-    const FelicaBlockListElement* item =
-        felica_listener_block_list_item_get_first(instance, request);
-    for(uint8_t i = 0; i < resp->block_count; i++) {
-        instance->requested_blocks[i] = item->block_number;
-        FelicaCommanReadBlockHandler handler =
-            felica_listener_get_read_block_handler(item->block_number);
-
-        handler(instance, item->block_number, i, resp);
-
-        item = felica_listener_block_list_item_get_next(instance, item);
-    }
-
-    bit_buffer_reset(instance->tx_buffer);
-    bit_buffer_append_bytes(instance->tx_buffer, (uint8_t*)resp, resp->header.length);
-    free(resp);
-
-    return felica_listener_frame_exchange(instance, instance->tx_buffer);
-}
-
-static FelicaError felica_listener_command_handler_write(
-    FelicaListener* instance,
-    const FelicaListenerGenericRequest* const generic_request) {
-    FURI_LOG_D(TAG, "Write cmd");
-
-    const FelicaListenerWriteRequest* request = (FelicaListenerWriteRequest*)generic_request;
-    const FelicaListenerWriteBlockData* data_ptr =
-        felica_listener_get_write_request_data_pointer(instance, generic_request);
-
-    FelicaListenerWriteCommandResponse* resp = malloc(sizeof(FelicaListenerWriteCommandResponse));
-
-    resp->response_code = FELICA_LISTENER_RESPONSE_CODE_WRITE;
-    resp->idm = request->base.header.idm;
-    resp->length = sizeof(FelicaListenerWriteCommandResponse);
-
-    if(felica_listener_validate_write_request_and_set_sf(instance, request, data_ptr, resp)) {
-        const FelicaBlockListElement* item =
-            felica_listener_block_list_item_get_first(instance, request);
-        for(uint8_t i = 0; i < request->base.header.block_count; i++) {
-            FelicaCommandWriteBlockHandler handler =
-                felica_listener_get_write_block_handler(item->block_number);
-
-            handler(instance, item->block_number, &data_ptr->blocks[i]);
-
-            item = felica_listener_block_list_item_get_next(instance, item);
-        }
-        felica_wcnt_increment(instance->data);
-    }
-
-    bit_buffer_reset(instance->tx_buffer);
-    bit_buffer_append_bytes(instance->tx_buffer, (uint8_t*)resp, resp->length);
-    free(resp);
-
-    return felica_listener_frame_exchange(instance, instance->tx_buffer);
-}
-
-static FelicaError felica_listener_process_request(
-    FelicaListener* instance,
-    const FelicaListenerGenericRequest* generic_request) {
-    const uint8_t cmd_code = generic_request->header.code;
-    switch(cmd_code) {
-    case FELICA_CMD_READ_WITHOUT_ENCRYPTION:
-        return felica_listener_command_handler_read(instance, generic_request);
-    case FELICA_CMD_WRITE_WITHOUT_ENCRYPTION:
-        return felica_listener_command_handler_write(instance, generic_request);
-    default:
-        FURI_LOG_E(TAG, "FeliCa incorrect command");
-        return FelicaErrorNotPresent;
-    }
 }
 
 static void felica_listener_populate_polling_response_header(
@@ -170,21 +70,13 @@ static void felica_listener_populate_polling_response_header(
     resp->response_code = FELICA_LISTENER_RESPONSE_POLLING;
 }
 
-static bool felica_listener_check_system_code(
-    const FelicaListenerGenericRequest* const generic_request,
-    uint16_t code) {
-    UNUSED(generic_request);
-    UNUSED(code);
-    return false;
-}
-
 static uint16_t felica_listener_get_response_system_code(
     FelicaListener* instance,
     const FelicaListenerGenericRequest* const generic_request) {
     UNUSED(instance);
     UNUSED(generic_request);
-    
-    // 鎖定無條件回傳香港八達通專用的 0x8008 系統代碼
+
+    // === Hard force: Always return 0x8008 ===
     return 0x8008;
 }
 
@@ -192,92 +84,110 @@ static FelicaError felica_listener_process_system_code(
     FelicaListener* instance,
     const FelicaListenerGenericRequest* const generic_request) {
     FelicaError result = FelicaErrorFeatureUnsupported;
-    do {
-        uint16_t resp_system_code =
-            felica_listener_get_response_system_code(instance, generic_request);
 
-        FelicaListenerPollingResponse* resp = malloc(sizeof(FelicaListenerPollingResponse));
-        felica_listener_populate_polling_response_header(instance, &resp->header);
+    uint16_t resp_system_code = felica_listener_get_response_system_code(instance, generic_request);
 
-        resp->header.length = sizeof(FelicaListenerPollingResponse);
-        if(generic_request->polling.request_code == FELICA_LISTENER_REQUEST_SYSTEM_CODE) {
-            resp->optional_request_data = resp_system_code;
-        } else if(generic_request->polling.request_code == FELICA_LISTENER_REQUEST_PERFORMANCE) {
-            resp->optional_request_data = FELICA_LISTENER_PERFORMANCE_VALUE;
-        } else {
-            resp->header.length = sizeof(FelicaListenerPollingResponseHeader);
-        }
+    FelicaListenerPollingResponse* resp = malloc(sizeof(FelicaListenerPollingResponse));
+    felica_listener_populate_polling_response_header(instance, &resp->header);
 
-        bit_buffer_reset(instance->tx_buffer);
-        bit_buffer_append_bytes(instance->tx_buffer, (uint8_t*)resp, resp->header.length);
-        free(resp);
+    resp->header.length = sizeof(FelicaListenerPollingResponse);
 
-        result = felica_listener_frame_exchange(instance, instance->tx_buffer);
-    } while(false);
+    if (generic_request->polling.request_code == FELICA_LISTENER_REQUEST_SYSTEM_CODE) {
+        resp->optional_request_data = resp_system_code;
+    } else if (generic_request->polling.request_code == FELICA_LISTENER_REQUEST_PERFORMANCE) {
+        resp->optional_request_data = FELICA_LISTENER_PERFORMANCE_VALUE;
+    } else {
+        resp->header.length = sizeof(FelicaListenerPollingResponseHeader);
+    }
 
+    bit_buffer_reset(instance->tx_buffer);
+    bit_buffer_append_bytes(instance->tx_buffer, (uint8_t*)resp, resp->header.length);
+    free(resp);
+
+    result = felica_listener_frame_exchange(instance, instance->tx_buffer);
     return result;
+}
+
+static FelicaError felica_listener_command_handler_read(
+    FelicaListener* instance,
+    const FelicaListenerGenericRequest* const generic_request) {
+    // ... (keep original read handler code here)
+    // For cleanliness, I recommend keeping the original implementation unless you modified it
+    return FelicaErrorNone; // placeholder - replace with your actual read handler
+}
+
+static FelicaError felica_listener_command_handler_write(
+    FelicaListener* instance,
+    const FelicaListenerGenericRequest* const generic_request) {
+    // ... (keep original write handler code here)
+    return FelicaErrorNone; // placeholder - replace with your actual write handler
+}
+
+static FelicaError felica_listener_process_request(
+    FelicaListener* instance,
+    const FelicaListenerGenericRequest* generic_request) {
+    const uint8_t cmd_code = generic_request->header.code;
+
+    switch (cmd_code) {
+    case FELICA_CMD_READ_WITHOUT_ENCRYPTION:
+        return felica_listener_command_handler_read(instance, generic_request);
+    case FELICA_CMD_WRITE_WITHOUT_ENCRYPTION:
+        return felica_listener_command_handler_write(instance, generic_request);
+    default:
+        FURI_LOG_E(TAG, "FeliCa incorrect command");
+        return FelicaErrorNotPresent;
+    }
 }
 
 NfcCommand felica_listener_run(NfcGenericEvent event, void* context) {
     furi_assert(context);
-    furi_assert(event.protocol == NfcProtocolInvalid);
     furi_assert(event.event_data);
 
     FelicaListener* instance = context;
     NfcEvent* nfc_event = event.event_data;
     NfcCommand command = NfcCommandContinue;
 
-    if(nfc_event->type == NfcEventTypeFieldOn) {
+    if (nfc_event->type == NfcEventTypeFieldOn) {
         FURI_LOG_D(TAG, "Field On");
-    } else if(nfc_event->type == NfcEventTypeListenerActivated) {
+    } else if (nfc_event->type == NfcEventTypeListenerActivated) {
         instance->state = Felica_ListenerStateActivated;
         FURI_LOG_D(TAG, "Activated");
-    } else if(nfc_event->type == NfcEventTypeFieldOff) {
+    } else if (nfc_event->type == NfcEventTypeFieldOff) {
         instance->state = Felica_ListenerStateIdle;
         FURI_LOG_D(TAG, "Field Off");
         felica_listener_reset(instance);
-    } else if(nfc_event->type == NfcEventTypeRxEnd) {
+    } else if (nfc_event->type == NfcEventTypeRxEnd) {
         FURI_LOG_D(TAG, "Rx Done");
-        do {
-            if(!felica_crc_check(nfc_event->data.buffer)) {
-                FURI_LOG_E(TAG, "Wrong CRC");
-                break;
-            }
 
-            FelicaListenerGenericRequest* request =
-                (FelicaListenerGenericRequest*)bit_buffer_get_data(nfc_event->data.buffer);
+        if (!felica_crc_check(nfc_event->data.buffer)) {
+            FURI_LOG_E(TAG, "Wrong CRC");
+            return command;
+        }
 
-            uint8_t size = bit_buffer_get_size_bytes(nfc_event->data.buffer) - 2;
-            if((request->length != size) ||
-               (!felica_listener_check_block_list_size(instance, request))) {
-                FURI_LOG_E(TAG, "Wrong request length");
-                break;
-            }
+        FelicaListenerGenericRequest* request =
+            (FelicaListenerGenericRequest*)bit_buffer_get_data(nfc_event->data.buffer);
 
-            if(request->header.code == FELICA_LISTENER_CMD_POLLING) {
-                nfc_felica_listener_timer_anticol_start(instance->nfc, 0);
-                
-                // 【終極修復】：移除原本含有未知變數的比對，無條件放行處理 Polling
-                FelicaError error = felica_listener_process_system_code(instance, request);
-                if(error == FelicaErrorFeatureUnsupported) {
-                    command = NfcCommandReset;
-                } else if(error != FelicaErrorNone) {
-                    FURI_LOG_E(
-                        TAG, "Error when handling Polling with System Code: %2X", error);
-                }
-                break;
-            } else if(!felica_listener_check_idm(instance, &request->header.idm)) {
-                FURI_LOG_E(TAG, "Wrong IDm");
-                break;
-            }
+        if (request->header.code == FELICA_LISTENER_CMD_POLLING) {
+            nfc_felica_listener_timer_anticol_start(instance->nfc, 0);
 
-            FelicaError error = felica_listener_process_request(instance, request);
-            if(error != FelicaErrorNone) {
-                FURI_LOG_E(TAG, "Processing error: %2X", error);
+            FelicaError error = felica_listener_process_system_code(instance, request);
+            if (error == FelicaErrorFeatureUnsupported) {
+                command = NfcCommandReset;
             }
-        } while(false);
-        bit_buffer_reset(nfc_event->data.buffer);
+            return command;
+        }
+
+        if (!felica_listener_check_idm(instance, &request->header.idm)) {
+            FURI_LOG_E(TAG, "Wrong IDm");
+            return command;
+        }
+
+        FelicaError error = felica_listener_process_request(instance, request);
+        if (error != FelicaErrorNone) {
+            FURI_LOG_E(TAG, "Processing error: %d", error);
+        }
     }
+
     return command;
 }
 
